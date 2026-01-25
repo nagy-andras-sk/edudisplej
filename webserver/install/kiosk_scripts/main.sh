4375302ff0192cf5e0af45436674472440955678
03Andras <3nagyandras@gmail.com>
2026-01-25 18:09:06 +0100

    20260125

--- webserver/install/kiosk_scripts/main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Example kiosk script
-# This is an example script that will be deployed to /opt/edudisplej/kiosk_scripts/
-
-echo "Example kiosk script executed"
-echo "This script is located at: $(dirname "${BASH_SOURCE[0]}")"
